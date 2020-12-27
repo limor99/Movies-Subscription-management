bc1545c916ba4d76d1562f4f85bfe2c0a78665e6
Limor Segal <segal.limor@gmail.com>
2020-12-27 14:48:12 +0200

    session timeout feature

--- CinemaWebSite/middlewares/checkPermissions.js
+++ CinemaWebSite/middlewares/checkPermissions.js
@@ -1,18 +1,26 @@
+/**
+ * This function check user's permission for doing action in the system.
+ * If user login check his permissions.
+ * If user not login (session tomeout) go to login page
+ * @param {*} permission 
+ */
 const checkPermissions = function (permission){
     return function(req, res, next){
         let user = req.session.user;
         
         if(user != null){
             userPermissions = user.permissions;
-        }
 
-        if(userPermissions.includes(permission)){
-            next();
+            if(userPermissions != null && userPermissions.includes(permission)){
+                next();
+            }
+            else{
+                res.render('main', {title: "Main Page", message: `You have no permission for ${permission}`});
+            }
         }
-        else{
-            res.render('main', {title: "Main Page", message: `You have no permission for ${permission}`});
+        else{   //for session timeout
+            res.render('login', {data : 'Login Page', message: 'Session timeout, please login'})
         }
-    
     }
 
 }
